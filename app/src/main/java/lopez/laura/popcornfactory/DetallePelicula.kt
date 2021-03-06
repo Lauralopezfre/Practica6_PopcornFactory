a6c780e920663d1041f8bacf1b4d3eea9d63dedc
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-03-06 12:40:04 -0700

    Inicialización de la practica

---
+++ app/src/main/java/lopez/laura/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,26 @@
+package lopez.laura.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+
+        var bundle = intent.extras
+
+        if(bundle != null) {
+            var image: ImageView = findViewById(R.id.lv_pelicula_imagen) as ImageView
+            var txNombrePelicula: TextView = findViewById(R.id.tv_nombre_pelicula) as TextView
+            var txPeliculaDesc: TextView = findViewById(R.id.tv_pelicula_desc) as TextView
+
+            image.setImageResource(bundle.getInt("header"))
+            txNombrePelicula.setText(bundle.getString("nombre"))
+            txPeliculaDesc.setText(bundle.getString("sinopsis"))
+        }
+    }
+}
