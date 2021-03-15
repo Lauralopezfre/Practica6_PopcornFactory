931e74c21f964c1b37777bf0d7d45daedd4e4a3b
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-03-15 10:46:27 -0700

    Creación de los tickets

---
+++ app/src/main/java/lopez/laura/popcornfactory/Cliente.kt
@@ -0,0 +1,5 @@
+package lopez.laura.popcornfactory
+
+data class Cliente (var nombre:String,
+                    var tipoPago:String){
+}
--- app/src/main/java/lopez/laura/popcornfactory/DetalleContenido.kt
+++ app/src/main/java/lopez/laura/popcornfactory/DetalleContenido.kt
@@ -17,10 +17,15 @@ class DetalleContenido : AppCompatActivity() {
             var image: ImageView = findViewById(R.id.lv_imagen) as ImageView
             var txNombrePelicula: TextView = findViewById(R.id.tv_nombre) as TextView
             var txPeliculaDesc: TextView = findViewById(R.id.tv_desc) as TextView
+            var movieSeatsLeft: TextView = findViewById(R.id.seatsLeft) as TextView
+
+            var ns = bundle.getInt("numberSeats")
 
             image.setImageResource(bundle.getInt("header"))
             txNombrePelicula.setText(bundle.getString("titulo"))
             txPeliculaDesc.setText(bundle.getString("sinopsis"))
+            movieSeatsLeft.setText("$ns setas available")
+
         }
     }
 }
